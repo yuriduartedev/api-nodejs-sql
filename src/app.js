b046cdd834307a4474e5eb2ecdb319c95c1993b7
Yuri Duarte <yuriduartedev@gmail.com>
2020-04-05 18:45:20 -0300

    Adiciona tratamento de erros na API

--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
 const productsRouter = require('./routes/products');
 const ordersRouter = require('./routes/orders');
 
+app.use(morgan('dev'));
 
 app.use('/products', productsRouter);
 app.use('/orders', ordersRouter);
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
